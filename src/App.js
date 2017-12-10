a82542388e5978e24a78a15190b68597cf9abc81
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2017-12-10 23:11:24 +0200

    feat: add column

--- src/App.js
+++ src/App.js
@@ -1,20 +1,54 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
+import {Container, Button, Segment, Grid} from 'semantic-ui-react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      columns: [
+        {
+          name: "Initial column"
+        }
+      ]
+    }
+  }
+
+  addColumn = () => {
+    this.setState({
+      columns: [
+        ...this.state.columns, {
+          name: "Column"
+        }
+      ]
+    })
+  }
+
   render() {
+    const cols = this
+      .state
+      .columns
+      .map(column => {
+        return <Grid.Column>
+          <p>{column.name}</p>
+        </Grid.Column>
+      })
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
+      <div>
+        <Grid columns={cols.length + 1} divided>
+          {cols}
+          <Grid.Column>
+            <Button
+              onClick={this.addColumn}
+              primary
+              content='Add column'
+              icon='plus'
+              labelPosition='left'/>
+          </Grid.Column>
+        </Grid>
       </div>
     );
   }
