bdfa49d444d9487c4f8f264ebd6d79139b55d8b4
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2017-12-12 00:26:49 +0200

    chrome: redux dev tools

--- src/index.js
+++ src/index.js
@@ -8,7 +8,10 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import boardApp from './store/reducers';
 
-let store = createStore(boardApp)
+let store = createStore(
+  boardApp,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 render(
     <Provider store={store}>
