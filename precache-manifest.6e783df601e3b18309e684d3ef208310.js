869087ffccc17e74154f0f1469c8b166cd3a407b
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2019-02-17 19:30:13 +0200

    Updates

---
+++ precache-manifest.6e783df601e3b18309e684d3ef208310.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ad65da4c48ec127e288",
+    "url": "/app-project-boards/static/js/runtime~main.2ad65da4.js"
+  },
+  {
+    "revision": "f5496f1536c749d2bb8f",
+    "url": "/app-project-boards/static/js/main.f5496f15.chunk.js"
+  },
+  {
+    "revision": "ca89e1142cb1d402a720",
+    "url": "/app-project-boards/static/js/1.ca89e114.chunk.js"
+  },
+  {
+    "revision": "f5496f1536c749d2bb8f",
+    "url": "/app-project-boards/static/css/main.0a3c1dd2.chunk.css"
+  },
+  {
+    "revision": "84074689b9afc3df871dd7fd27093651",
+    "url": "/app-project-boards/index.html"
+  }
+];
