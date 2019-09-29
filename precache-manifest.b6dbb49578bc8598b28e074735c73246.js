f7b8206ea5421f83e25df4e64e5b480908023fab
vytautassugintas <vytautas.sugintas@gmail.com>
2019-09-29 19:49:28 +0300

    Updates

---
+++ precache-manifest.b6dbb49578bc8598b28e074735c73246.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b8dfdd57752d5d7ad01cd9e57b2e6b4",
+    "url": "/app-project-boards/index.html"
+  },
+  {
+    "revision": "ce64c63c7c19d5ccf09f",
+    "url": "/app-project-boards/static/css/main.e8fe9d14.chunk.css"
+  },
+  {
+    "revision": "55d90bcf6edc3a073189",
+    "url": "/app-project-boards/static/js/2.03b73832.chunk.js"
+  },
+  {
+    "revision": "ce64c63c7c19d5ccf09f",
+    "url": "/app-project-boards/static/js/main.fd14905f.chunk.js"
+  },
+  {
+    "revision": "9eab47227afa857f94a6",
+    "url": "/app-project-boards/static/js/runtime-main.8a985db6.js"
+  }
+]);
